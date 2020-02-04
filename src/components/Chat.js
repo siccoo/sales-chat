1ec05944f04701a99833c9687404d8e2d3727015
siccoo <obichilaka@gmail.com>
2020-02-04 15:34:35 +0100

    First commit on Chat-App

---
+++ src/components/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+const Chat = () => {
+    return (
+        <h1>Chat Page</h1>
+    )
+}
+
+export default Chat;
---
+++ src/components/Join.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+const Join = () => {
+    return (
+        <h1>Join Page</h1>
+    )
+}
+
+export default Join;
+
