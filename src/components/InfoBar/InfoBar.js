b614893c8d2e0d6254ce5f741f8ddf341ed6607a
siccoo <obichilaka@gmail.com>
2020-02-06 14:46:24 +0100

    icons folder created

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 import './InfoBar.css';
 
-const InfoBar = () => {
+import closeIcon from '../../icons/closeIcon';
+import onlineIcon from '../../icons/onlineIcon';
 
+const InfoBar = () => {
+    <div className="infoBar">
+        <div className="leftInnerContainer">
+            <img className="onlineIcon" src={onlineIcon} alt="online image" />
+            <h3>roomName</h3>
+        </div>
+        <div className="rightInnerContainer">
+            <a href="/"><img src={closeIcon} alt="close image" /></a>
+        </div>
+    </div>
 }
 
 export default InfoBar;
