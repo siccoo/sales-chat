06dc777b4bceefb5cb173f34662c98ef9e6d1303
siccoo <obichilaka@gmail.com>
2020-02-07 13:18:50 +0100

    Text container folder added

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client'; 
 
-import './Chat.css'; 
+import TextContainer from '../TextContainer/TextContainer';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
 import Messages from '../Messages/Messages';
+
+import './Chat.css'; 
 
 let socket;
 
 const Chat = ({ location }) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [users, setUsers] = useState('');
     const [message, setMessage] = useState();
     const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:5000';
@@ -40,6 +43,16 @@ const Chat = ({ location }) => {
         socket.on('message', (message) => {
             setMessages([...messages, message]);
         })
+
+        socket.on('roomData', ({ users }) => {
+            setUsers(users);
+          })
+      
+          return () => {
+            socket.emit('disconnect');
+      
+            socket.off();
+          }
     }, [messages]);
 
 // FUNCTION FOR SENDING MESSAGES
@@ -65,6 +78,7 @@ const Chat = ({ location }) => {
                     onChange={(event) => setMessage(event.target.value)} 
                     onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null} /> */}
             </div>
+            <TextContainer users={users} />
         </div>
     )
 };
