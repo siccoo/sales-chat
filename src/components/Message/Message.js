3762f162e24207c02e307fdae3f996c182104b63
siccoo <obichilaka@gmail.com>
2020-02-07 04:27:47 +0100

    Structure of the Message.js

---
+++ src/components/Message/Message.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './Message.css';
+
+const Message = ({ message: { user, text }, name }) => {
+    let isSentByCurrentUser = false;
+
+    const trimmedName = name.trim().toLowerCase();
+
+    if(user === trimmedName) {
+        isSentByCurrentUser = true;
+    }
+
+    return (
+        isSentByCurrentUser
+            ? (
+                <div className="messageContainer justifyEnd">
+                    <p className="sentText">{trimmedName}</p>
+                    <div className="messageBox">
+                        <p className="messageText">{text}</p>
+                    </div>
+                </div>
+            ) : (
+                <div className="messageContainer justifyStart">
+                    <p className="sentText">{trimmedName}</p>
+                    <div className="messageBox">
+                        <p className="messageText">{text}</p>
+                    </div>
+                </div>
+            )
+    )
+}
+
+export default Message;
