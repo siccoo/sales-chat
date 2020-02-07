0023f62e5d03e40e81faf01ae0f4b10037870ec4
siccoo <obichilaka@gmail.com>
2020-02-07 14:12:22 +0100

    connected our backend to frontend through endpoint

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState();
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://sales-chat.herokuapp.com/';
     // We wont be needing localhost:5000 again, we will put the URL gotten from the backend server as our ENDPOINT
 
     useEffect(() => {
