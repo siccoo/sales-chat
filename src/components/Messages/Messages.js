55c0a3502f3375edcaa958d16dd7dd6e93c88c1f
siccoo <obichilaka@gmail.com>
2020-02-07 04:55:08 +0100

    Styling of the Message.css

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
 
+import Message from '../Message/Message';
+
 import './Messages.css';
+
 
 const Messages = ({ messages, name }) => (
     <ScrollToBottom>
